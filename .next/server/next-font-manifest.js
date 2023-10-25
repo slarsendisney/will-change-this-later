6369e29e5921101d7e3ff0d5aa28b726b5b93d25
Mynameistessa <tessabreen@protonmail.com>
2023-10-25 10:24:16 +0200

    Add widget and messenger api

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/tessabreen/development/workspace/sick-hack/will-change-this-later/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
