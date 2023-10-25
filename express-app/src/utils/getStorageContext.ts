8be2caadc093f391de951580b045fe87c111cb12
slarsendisney <s.larsendisney@gmail.com>
2023-10-25 21:21:39 +0200

    I heard you like indexes

--- express-app/src/utils/getStorageContext.ts
+++ express-app/src/utils/getStorageContext.ts
@@ -1,11 +1,9 @@
 import { storageContextFromDefaults } from "llamaindex";
-import path from "path";
 
 export const getStorageContext = async () => {
   console.log("💾 Creating or loading storage context");
-  //storage should use var/data in the root of the project
   const storageContext = await storageContextFromDefaults({
-    persistDir: path.join("/var", "data"),
+    persistDir: "./src/storage",
   });
 
   return storageContext;
